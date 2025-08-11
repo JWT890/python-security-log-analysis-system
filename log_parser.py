603ca01e8e55a2920b48e2fd639a9796e5336446
JWT890 <jwtaylor659@gmail.com>
2025-08-11 13:48:21 -0500

    report_generation

--- log_parser.py
+++ log_parser.py
@@ -17,8 +17,26 @@ def analyze_log():
 
 
 
-def generate_report(output_file='incident_report.txt'):
+def generate_report(incidents, output_file='incident_report.txt'):
+    if not incidents:
+        print("No suspicious activites detected. No report detected")
+        return
+    with open(output_file, 'w') as f:
+        f.write("====================================================================\n")
+        f.write("Security Incident Report\n")
+        f.write("====================================================================\n")
+        f.write(f"Report Generated: {datetime.now().isoformat{}}\n")
+        f.write(f"Total Incident Detected: {len(incidents)}\n\n")
+        f.write("----------------------------------------------------------------------\n\m")
 
+        for i in incident in enumerate(incidents):
+            f.write(f"Incident #{i + 1}\n")
+            f.write(f'Timestamp: {incident['timestamp'].isoformat()}\n')
+            f.write(f"Rule Triggered: {incident[rule_name].replace('_', ' ').title()}\n")
+            f.write(f"Log Entry: {incident['log_entry']}\n")
+            f.write("----------------------------------------------------------------------\n\n")
+    
+    print(f"Report successfully generated: {output_file}")
 
 def main():
 
